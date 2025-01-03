841afeeef02e4feec3d3c63b200e50095cc6234f
Anne-Flore Bernard <139115974+Anne-Flower@users.noreply.github.com>
2025-01-03 17:11:36 +0100

    ex 1 and 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
+require_relative "rulers/array"
 
 require "rulers/version"
 module Rulers  
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [
         200,
         {'Content-Type' => 'text/html'},
